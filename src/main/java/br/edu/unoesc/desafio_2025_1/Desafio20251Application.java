f4df99e8b5e85a241c0fea4b1641b2493491a6e5
Vinicius Brunoni <viniciusbrunoni8@gmail.com>
2025-01-30 20:33:32 -0300

    first commit

---
+++ src/main/java/br/edu/unoesc/desafio_2025_1/Desafio20251Application.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc.desafio_2025_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio20251Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio20251Application.class, args);
+	}
+
+}
