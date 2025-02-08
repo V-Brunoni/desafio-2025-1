acf7ff727cd1bb4c8f3bb01407e44b5e637ab3d8
Vinicius Brunoni <viniciusbrunoni8@gmail.com>
2025-02-07 22:17:56 -0300

    Adicionando classes para controlar os endpoints

--- src/main/java/br/edu/unoesc/desafio_2025_1/controller/CursoController.java
+++ src/main/java/br/edu/unoesc/desafio_2025_1/controller/CursoController.java
@@ -1,4 +1,45 @@
 package br.edu.unoesc.desafio_2025_1.controller;
 
+import br.edu.unoesc.desafio_2025_1.model.Curso;
+import br.edu.unoesc.desafio_2025_1.model.Pessoa;
+import br.edu.unoesc.desafio_2025_1.service.CursoService;
+import br.edu.unoesc.desafio_2025_1.service.PessoaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/curso")
 public class CursoController {
+
+    @Autowired
+    CursoService cursoService;
+
+    @PostMapping("/salvar")
+    public ResponseEntity<?> salvarCurso(@Validated @ModelAttribute Curso curso, BindingResult result){
+        try {
+            if(result.hasErrors()){
+                Map<String, String> erros = new HashMap<>();
+                result.getFieldErrors().forEach(t->{
+                    erros.put(t.getField(), t.getDefaultMessage());
+                });
+                return ResponseEntity.badRequest().body(erros);
+            }
+            Curso cursoSalvo = cursoService.cadastrarCurso(curso);
+            return ResponseEntity.status(HttpStatus.CREATED).body(curso);
+        }catch (Exception e){
+            Map<String, String> erro = new HashMap<>();
+            erro.put("erro", "Ocorreu um problema ao Salvar o registro");
+            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(erro);
+        }
+    }
 }
--- src/main/java/br/edu/unoesc/desafio_2025_1/controller/PessoaController.java
+++ src/main/java/br/edu/unoesc/desafio_2025_1/controller/PessoaController.java
@@ -1,4 +1,59 @@
 package br.edu.unoesc.desafio_2025_1.controller;
 
+
+import br.edu.unoesc.desafio_2025_1.model.Pessoa;
+import br.edu.unoesc.desafio_2025_1.service.PessoaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/pessoa")
 public class PessoaController {
+
+    @Autowired
+    PessoaService pessoaService;
+
+    @GetMapping("/cadastro")
+    public String getPessoas(@ModelAttribute("pessoa") Pessoa pessoa){
+        return "/cadastros/cadastroPessoa";
+    }
+
+    @PostMapping("/salvar")
+    public ResponseEntity<?> salvarPessoa(@Validated @ModelAttribute Pessoa pessoa, BindingResult result){
+        try {
+            if(result.hasErrors()){
+                Map<String, String> erros = new HashMap<>();
+                result.getFieldErrors().forEach(t->{
+                    erros.put(t.getField(), t.getDefaultMessage());
+                });
+                return ResponseEntity.badRequest().body(erros);
+            }
+            Pessoa pessoaSalva = pessoaService.cadastrarPessoa(pessoa);
+            return ResponseEntity.status(HttpStatus.CREATED).body(pessoa);
+        }catch (Exception e){
+            Map<String, String> erro = new HashMap<>();
+            erro.put("erro", "Ocorreu um problema ao Salvar o registro");
+            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(erro);
+        }
+    }
+
+
+
 }
+
+
+
+
+
+
